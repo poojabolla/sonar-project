962029d2f6382395dcfa6da386be23be1dbccf4c
Edureka <Edureka@edureka.co>
2025-07-16 08:21:31 +0000

    updated code for sonar scan

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,9 +3,9 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppTest {
+ class AppTest { //public removed
     @Test
-    public void testAdd() {
+    void testAdd() {
         App app = new App();
         assertEquals(5, app.add(2, 3));
     }
